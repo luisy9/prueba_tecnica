f9cbb38e4b6093fde22c1296433d28c84c3d5e45
luisy9 <luisdeharo4@gmail.com>
2024-04-13 15:46:17 +0200

    Validacion del input Author

--- resources/views/authors/create.blade.php
+++ resources/views/authors/create.blade.php
@@ -1,26 +1,25 @@
 <x-layout>
     <div class="container">
         <h1 class="">Add new Author</h1>
-        @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
+        
 
-        @if (session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        @endif
-        <form method="POST" action="{{ route('author.create') }}">
+        @if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+        <form method="POST" action="{{ route('author.create') }}" class="needs-validation">
             @csrf
-            <div class="container">
+            <div class="container has-validation">
                 <label for="inputName" class="form-label">
                     Name
                 </label>
-                <input type="text" name="name" id="inputName" class="form-control" /> 
-                <button class="bg-primary px-4 py-2 text-white border border-0 rounded my-2">Submit</button>
-            </div>
+                <input type="text" name="name" id="inputName" class="form-control @if(session('error')) is-invalid @endif" /> 
+                <div id="validationServer05Feedback" class="invalid-feedback">
+                    {{session('error')}}
+                </div>
+                
+                <button class="bg-primary px-4 py-2 text-white border border-0 rounded my-2" type="submit">Submit</button>
             </div>
         </form>
     </div>
